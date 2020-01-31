83b1ee758e4ba7d07f642e7014e70c78bdab7520
Vincent Zhao <gyroknight@users.noreply.github.com>
2020-01-30 19:11:00 -0500

    Removed print from Map

--- map.h
+++ map.h
@@ -1,64 +1,61 @@
-//lang::CwC
+// lang::CwC
 #pragma once
 
-#include "object.h"
 #include "list.h"
+#include "object.h"
 
 // Represents a mapping from Objects to Objects based on their hashcodes. Much
 // of the interface comes from Java's HashMap.
 class Map : public Object {
-   public:
-    // Construct an empty map
-    Map();
+ public:
+  // Construct an empty map
+  Map();
 
-    // Free memory for map
-    ~Map();
+  // Free memory for map
+  ~Map();
 
-    // Override. Print map
-    virtual void print();
+  // Override. Make a shallow copy of the map (does not clone keys and values)
+  virtual Map* clone();
 
-    // Override. Make a shallow copy of the map (does not clone keys and values)
-    virtual Map* clone();
+  // Override. Compare to other object
+  virtual bool equals(Object* other);
 
-    // Override. Compare to other object
-    virtual bool equals(Object* other);
+  // Override. Computer hash code for map
+  virtual size_t hash();
 
-    // Override. Computer hash code for map
-    virtual size_t hash();
+  // Returns true if this map contains the given key
+  bool containsKey(Object* key);
 
-    // Returns true if this map contains the given key
-    bool containsKey(Object* key);
+  // Returns true if this map contains the given value
+  bool containsValue(Object* value);
 
-    // Returns true if this map contains the given value
-    bool containsValue(Object* value);
+  // Return the object to which the given key is mapped, or nullptr is one
+  // does not exist
+  Object* get(Object* key);
 
-    // Return the object to which the given key is mapped, or nullptr is one
-    // does not exist
-    Object* get(Object* key);
+  // Return true if there are no keys in this map
+  bool isEmpty();
 
-    // Return true if there are no keys in this map
-    bool isEmpty();
+  // Map the given key to the given value in this map. Returns the previous
+  // value associated with that key, or nullptr if none.
+  Object* put(Object* key, Object* value);
 
-    // Map the given key to the given value in this map. Returns the previous
-    // value associated with that key, or nullptr if none.
-    Object* put(Object* key, Object* value);
+  // Copy all the mappings from the given map to this map
+  void putAll(Map* map);
 
-    // Copy all the mappings from the given map to this map
-    void putAll(Map* map);
+  // Removes the given key's mapping from this map. Returns the value
+  // associated with the key, or nullptr if none
+  Object* remove(Object* key);
 
-    // Removes the given key's mapping from this map. Returns the value
-    // associated with the key, or nullptr if none
-    Object* remove(Object* key);
+  // Returns the number of key-value mappings in this map
+  size_t size();
 
-    // Returns the number of key-value mappings in this map
-    size_t size();
+  // Clears all mappings
+  void clear();
 
-    // Clears all mappings
-    void clear();
+  // Returns a copy of the list of keys in this map
+  List* keys();
 
-    // Returns a copy of the list of keys in this map
-    List* keys();
-
-    // Returns a copy of the list of values in this map
-    List* values();
+  // Returns a copy of the list of values in this map
+  List* values();
 };
